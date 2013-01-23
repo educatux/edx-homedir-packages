efdacda0a88e74b1bac6818cfd07eeb125bdb6c4
Stéphane GAUTIER <educatux@gmail.com>
2013-01-23 07:39:18 +0100

    Update add_routes script

--- common-tools/.bin/add_routes.sh
+++ common-tools/.bin/add_routes.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-sudo route add -net 192.168.0.0/16 gw 172.31.12.10
-sudo route add -net 10.0.0.0/8 gw 172.31.12.10
-sudo route add -net 172.16.0.0/12 gw 172.31.12.10
+sudo route add -net 192.168.0.0/16 gw 192.168.100.10
+sudo route add -net 10.0.0.0/8 gw 192.168.100.10
+sudo route add -net 172.16.0.0/12 gw 192.168.100.10
