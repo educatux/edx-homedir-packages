19c540c4d8663f57421b1a433164d60d450ccf95
Stéphane GAUTIER <stephane@educatux.org>
2015-05-14 18:44:36 +0200

    Suppression des Modules migrés en HomeSick

--- common-tools/.bin/add_routes.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-sudo iptables -A OUTPUT -p tcp -d 173.194.52.0/22 -j REJECT --reject-with tcp-reset
-sudo route add -net 192.168.0.0/16 gw 192.168.100.10
-sudo route add -net 10.0.0.0/8 gw 192.168.100.10
-sudo route add -net 172.16.0.0/12 gw 192.168.100.10
--- common-tools/.bin/consoleSpeedTest.bash
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-for x in $(seq 10000) ; do
-    echo -e "#$x - \033[0;33mcoucou\033[1;39m, Ceci est un test\033[0m"
-done
